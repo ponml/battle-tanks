b28ad6b223c26f37ff111f1b9227c1f52d39afe4
chris <ccsand@hotmail.com>
2018-05-07 08:46:41 -0700

    tank class/playercontroller

---
+++ BattleTanks/Source/BattleTanks/Public/TankPlayerController.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tank.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" //must be the last include for unreal
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ATank* GetControlledTank() const; //as a getter, const makes sense
+
+
+
+
+	
+};
